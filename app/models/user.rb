007e33e9ebc1fe53c79ec899036cc3013bb17698
Tomer Mintz <tomer.mintz@gmail.com>
2016-07-23 20:44:39 +0300

    Associations Pin/User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :pins
+
+
 	validates :password, :presence => true,
      	:on => :create,
      	:format => {:with => /\A.*(?=.{8,})(?=.*\d)(?=.[a-z]).*\Z/ }
